b62b013dc7cefccea60e3d831ef89be4685c8b97
NathanFallet <contact@nathanfallet.me>
2023-12-06 19:26:22 +0100

    First controller/router

--- extopy-backend/src/commonMain/kotlin/me/nathanfallet/extopy/plugins/Koin.kt
+++ extopy-backend/src/commonMain/kotlin/me/nathanfallet/extopy/plugins/Koin.kt
@@ -4,17 +4,23 @@ import io.ktor.server.application.*
 import me.nathanfallet.extopy.controllers.auth.AuthRouter
 import me.nathanfallet.extopy.controllers.notifications.NotificationsRouter
 import me.nathanfallet.extopy.controllers.posts.PostsRouter
+import me.nathanfallet.extopy.controllers.users.UsersController
 import me.nathanfallet.extopy.controllers.users.UsersRouter
 import me.nathanfallet.extopy.database.Database
 import me.nathanfallet.extopy.database.users.DatabaseUsersRepository
 import me.nathanfallet.extopy.models.auth.LoginPayload
 import me.nathanfallet.extopy.models.auth.RegisterCodePayload
 import me.nathanfallet.extopy.models.auth.RegisterPayload
+import me.nathanfallet.extopy.models.users.CreateUserPayload
+import me.nathanfallet.extopy.models.users.UpdateUserPayload
+import me.nathanfallet.extopy.models.users.User
 import me.nathanfallet.extopy.repositories.users.IUsersRepository
 import me.nathanfallet.extopy.usecases.auth.CreateCodeRegisterUseCase
 import me.nathanfallet.extopy.usecases.auth.GetCodeRegisterUseCase
 import me.nathanfallet.extopy.usecases.auth.RegisterUseCase
+import me.nathanfallet.extopy.usecases.users.GetUserForCallUseCase
 import me.nathanfallet.i18n.usecases.localization.TranslateUseCase
+import me.nathanfallet.ktorx.controllers.IModelController
 import me.nathanfallet.ktorx.controllers.auth.AuthWithCodeController
 import me.nathanfallet.ktorx.controllers.auth.IAuthWithCodeController
 import me.nathanfallet.ktorx.usecases.auth.ICreateCodeRegisterUseCase
@@ -22,9 +28,13 @@ import me.nathanfallet.ktorx.usecases.auth.IGetCodeRegisterUseCase
 import me.nathanfallet.ktorx.usecases.auth.IRegisterUseCase
 import me.nathanfallet.ktorx.usecases.localization.GetLocaleForCallUseCase
 import me.nathanfallet.ktorx.usecases.localization.IGetLocaleForCallUseCase
+import me.nathanfallet.ktorx.usecases.users.IGetUserForCallUseCase
 import me.nathanfallet.ktorx.usecases.users.IRequireUserForCallUseCase
 import me.nathanfallet.ktorx.usecases.users.RequireUserForCallUseCase
 import me.nathanfallet.usecases.localization.ITranslateUseCase
+import me.nathanfallet.usecases.models.get.context.GetModelWithContextFromRepositorySuspendUseCase
+import me.nathanfallet.usecases.models.get.context.IGetModelWithContextSuspendUseCase
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
@@ -62,8 +72,13 @@ fun Application.configureKoin() {
 
             // Users
             single<IRequireUserForCallUseCase> { RequireUserForCallUseCase(get()) }
+            single<IGetUserForCallUseCase> { GetUserForCallUseCase() }
+            single<IGetModelWithContextSuspendUseCase<User, String>>(named<User>()) {
+                GetModelWithContextFromRepositorySuspendUseCase(get<IUsersRepository>())
+            }
         }
         val controllerModule = module {
+            // Auth
             single<IAuthWithCodeController<LoginPayload, RegisterPayload, RegisterCodePayload>> {
                 AuthWithCodeController(
                     get(),
@@ -75,10 +90,18 @@ fun Application.configureKoin() {
                     get()
                 )
             }
+
+            // Users
+            single<IModelController<User, String, CreateUserPayload, UpdateUserPayload>>(named<User>()) {
+                UsersController(
+                    get(),
+                    get(named<User>())
+                )
+            }
         }
         val routerModule = module {
             single { AuthRouter(get(), get()) }
-            single { UsersRouter() }
+            single { UsersRouter(get(named<User>())) }
             single { NotificationsRouter() }
             single { PostsRouter() }
         }
--- extopy-backend/src/commonMain/kotlin/me/nathanfallet/extopy/plugins/Routing.kt
+++ extopy-backend/src/commonMain/kotlin/me/nathanfallet/extopy/plugins/Routing.kt
@@ -4,18 +4,19 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.http.content.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import me.nathanfallet.extopy.controllers.users.UsersRouter
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
     routing {
-        // Static files
-        get("/") { call.respond(FreeMarkerContent("index.ftl", null)) }
+        val usersRouter by inject<UsersRouter>()
 
         authenticate("api-v1-jwt", optional = true) {
-
+            usersRouter.createRoutes(this)
         }
 
         staticResources("", "static")
+        get { call.respondTemplate("index.ftl") }
     }
 }
