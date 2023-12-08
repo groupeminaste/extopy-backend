af8d0bca81ccddeeda8a544ec40fc88dbe33fe57
NathanFallet <contact@nathanfallet.me>
2023-12-08 18:42:28 +0100

    Oups

--- extopy-commons/src/commonMain/kotlin/me/nathanfallet/extopy/models/auth/RegisterCodePayload.kt
+++ extopy-commons/src/commonMain/kotlin/me/nathanfallet/extopy/models/auth/RegisterCodePayload.kt
@@ -2,11 +2,15 @@ package me.nathanfallet.extopy.models.auth
 
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
+import me.nathanfallet.extopy.models.users.User
+import me.nathanfallet.usecases.models.annotations.validators.StringPropertyValidator
 
 @Serializable
 data class RegisterCodePayload(
     val password: String,
+    @StringPropertyValidator(regex = User.USERNAME_REGEX, maxLength = 25)
     val username: String,
+    @StringPropertyValidator(maxLength = 40)
     val displayName: String,
     val birthdate: LocalDate,
 )
